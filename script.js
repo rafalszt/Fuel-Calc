08307e1b3b5db9bbe4176dee011765b57cce43bf
Rafal Szternel <szternelrafal@gmail.com>
2020-10-14 22:21:39 +0200

    add second calc.

--- script.js
+++ script.js
@@ -6,9 +6,18 @@ const gasError = document.querySelector('.gas-error');
 const gasInfo = document.querySelector('.gas-info');
 const gasValue = document.querySelector('.gas-value');
 const option = document.querySelector('.option');
-// const gas = document.querySelector('#gas');
 
-const checkDate =()=>{
+
+const trip = document.querySelector('#trip');
+const fuelCons = document.querySelector('#fuel-cons');
+const fuelPrice = document.querySelector('#fuel-price');
+const btnCostCount = document.querySelector('.cost-count');
+const fuelError = document.querySelector('.fuel-error');
+const fuelInfo = document.querySelector('.fuel-info');
+
+
+
+const fuelCalc =()=>{
 	if (gas.value == '' || km.value ==''){
 		gasError.textContent = "Uzupełnij dane!";
 	}else if(gas.value !== '' && km.value !=='' && gasPrice.value ===""){
@@ -57,4 +66,30 @@ const optionValue=()=>{
 	
 }
 
-btnGasCount.addEventListener('click', checkDate)
+const priceCalc =()=>{
+	if (trip.value =='' || fuelCons.value == '' || fuelPrice.value == ''){
+		fuelError.textContent = `Proszę o uzupełnienie wszystkich danych!.`;
+	}else {
+		tripCost();
+	}
+}
+
+const tripCost=()=>{
+	const newTrip = parseFloat(trip.value);
+	const newFuelCons = parseFloat(fuelCons.value);
+	const newFuelPrice = parseFloat(fuelPrice.value);
+
+	const sum = ((newTrip*newFuelCons)/100)*newFuelPrice;
+	fuelInfo.textContent = `Koszt przejazdu ${newTrip} km wyniesie ${sum} zł.`;
+
+
+
+
+	document.getElementById('trip').value = "";
+	document.getElementById('fuel-cons').value = "";
+	document.getElementById('fuel-price').value = "";
+
+}
+
+btnGasCount.addEventListener('click', fuelCalc);
+btnCostCount.addEventListener('click', priceCalc)
